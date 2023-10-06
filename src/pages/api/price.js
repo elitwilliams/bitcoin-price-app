2c905d3996585e611e9de61036eeb1af30a0eacf
Eli Williams <eli.t.williams@gmail.com>
2023-10-05 21:53:06 -0400

    working version

--- src/pages/api/price.js
+++ src/pages/api/price.js
@@ -1,5 +1,5 @@
 export default async (_, res) => {
-    const result = await fetch("https://api.coindesk.com/v1/bpi/currentprice/bitcoin.json");
+    const result = await fetch("https://api.coindesk.com/v1/bpi/currentprice/btc.json");
     const data = await result.json();
     res.status(200).json(data.bpi.USD.rate);
 };
